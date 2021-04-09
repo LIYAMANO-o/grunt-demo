1028d36140e863a5adade40700dc016eb8ff5a97
wangding <1771375498@qq.com>
2021-04-09 15:00:36 +0800

    imagemin

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+   grunt.initConfig({
+     imagemin:{
+       dynamic:{
+         files:[{
+           expand:true,
+           src:['image/*.{png,jpg}'],
+           dest:'dist/'
+         }]
+       }
+     }
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  
+  grunt.registerTask('default', ['imagemin']);
+};
